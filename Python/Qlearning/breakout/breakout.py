bf40b286e89b553239d9ebc6c00978a6afb33ce3
Beansen MacBook <b.stinson90@gmx.de>
2018-01-16 21:16:46 +0100

    Added learning loop

--- Python/Qlearning/breakout/breakout.py
+++ Python/Qlearning/breakout/breakout.py
@@ -5,12 +5,11 @@ import os
 
 env = gym.make('Breakout-v0')
 
-episodes = 200
+episodes = 10
 stepsPerEpisode = 70
-discountFactor = 0.97
+discountFactor = 0.9
 chanceRandomAction = 0.1
 
-state = env.reset()[30:210]
 
 inputLayer = tf.placeholder(tf.float32, [180, 160, 3])
 
@@ -19,21 +18,33 @@ conv2 = tf.layers.conv2d(conv1, 5, [5, 5], strides=(2, 2))
 conv3 = tf.layers.conv2d(conv2, 10, [4, 5], strides=(2, 2))
 
 # possible to add dropout layers
-dense1 = tf.layers.dense(inputs=tf.reshape(conv3, [1, 42 * 36 * 10]), units=42 * 36 * 10, activation=tf.nn.relu)
+dense1 = tf.layers.dense(tf.reshape(conv3, [1, 42 * 36 * 10]), units=42 * 36 * 10, activation=tf.nn.relu)
 dense2 = tf.layers.dense(dense1, units=1024, activation=tf.nn.relu)
 logits = tf.layers.dense(dense2, units=4, activation=tf.nn.relu)
-predict = tf.argmax(logits, axis=0)
+predict = tf.argmax(logits[0])
+
+newQ = tf.placeholder(shape=[1, 4], dtype=tf.float32)
+loss = tf.reduce_sum(tf.square(newQ - logits))
+trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+updateModel = trainer.minimize(loss)
 
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-highestQ, allQ = sess.run([predict, logits], feed_dict={inputLayer: state})
+for i in range(episodes):
+	currentState = env.reset()[30:210] / 255.0
 
+	for k in range(stepsPerEpisode):
+		action, qValues = sess.run([predict, logits], feed_dict={inputLayer: currentState})
+		if np.random.rand(1) <= chanceRandomAction:
+			action = env.action_space.sample()
 
-# (1, 176, 156, 10)
-# (1, 86, 76, 5)
-# (1, 42, 36, 10)
-# (1, 15120)
-# (1, 1024)
-# (1, 4)
+		nextState, reward, done, info = env.step(action)
+
+		nextAction, nextQ = sess.run([predict, logits], feed_dict={inputLayer: nextState[30:210] / 255.0})
+		qValues[0, action] = reward + discountFactor * nextQ[0, nextAction]
+
+		sess.run([updateModel], feed_dict={inputLayer: currentState, newQ: qValues})
+
+		currentState = nextState[30:210]
