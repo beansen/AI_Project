7855dddd2d25f6e3a55baf86dac40d8d24830d9f
Beansen MacBook <b.stinson90@gmx.de>
2018-08-07 06:33:18 +0200

    Test images

--- Python/NN_SuperMario/main.py
+++ Python/NN_SuperMario/main.py
@@ -8,12 +8,13 @@ img_processor.init()
 
 temp = np.empty((3, 491))
 checkTemp = False
-
+imgCounter = 1
 while True:
 	img_np = np.array(ImageGrab.grab(bbox=(0, 50, 612, 525)))
-	grayScale = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-	cutout = grayScale[100:grayScale.shape[0]-1, 0:grayScale.shape[1]-1]
-
-	input_grid = img_processor.process_image(cutout)
-	nn.calculatefitness(cutout)
-
+	cv2.imwrite('mario' + imgCounter.__str__() + '.png', img_np)
+	imgCounter += 1
+# grayScale = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+# cutout = grayScale[100:grayScale.shape[0]-1, 0:grayScale.shape[1]-1]
+#
+# input_grid = img_processor.process_image(cutout)
+# nn.calculatefitness(cutout)
